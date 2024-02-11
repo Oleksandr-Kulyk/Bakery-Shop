22d801a509abdbcf238cbd4fcc64b6adf55238f1
Oleksandr Kulyk <mort36283@gmail.com>
2024-02-11 10:41:08 +0200

    Footer added

--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -2,13 +2,15 @@ const swiper = new Swiper('.swiper', {
     loop: true,
     slidesPerView: 3,
     spaceBetween: 20,
-
+  
+    // If we need pagination
     pagination: {
       el: '.pagination',
       bulletClass: 'pagination__button',
       bulletActiveClass: 'pagination__button_active',
     },
   
+    // Navigation arrows
     navigation: {
       nextEl: '.carousel-button-next',
       prevEl: '.carousel-button-prev',
@@ -17,5 +19,10 @@ const swiper = new Swiper('.swiper', {
     autoplay: {
       delay: 3000,
       pauseOnMouseEnter: true,
-    }
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
   });
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,20 +1,28 @@
 const swiper = new Swiper('.swiper', {
-    loop: true,
-    slidesPerView: 3,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.carousel-button-next',
-      prevEl: '.carousel-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+
+  // If we need pagination
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination__button',
+    bulletActiveClass: 'pagination__button_active',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.carousel-button-next',
+    prevEl: '.carousel-button-prev',
+  },
+
+  autoplay: {
+    delay: 3000,
+    pauseOnMouseEnter: true,
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
