35a54ed2bd7ab616d19f1dfe3f27a230933e8fcd
Oleksandr Kulyk <mort36283@gmail.com>
2024-02-13 20:55:37 +0200

    modal completed, mobile-menu added

--- dist/js/modal.js
+++ dist/js/modal.js
@@ -1,3 +1,5 @@
 const openButton = document.querySelector('.modal-open');
+const closeButton = document.querySelector('.modal-close-btn');
 const modal = document.querySelector('.backdrop');
 openButton.addEventListener('click', () => modal.classList.remove('isHidden') );
+closeButton.addEventListener ('click', () => modal.classList.add('isHidden'));
